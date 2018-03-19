0a60a492adcb41938a5f88cbf017739f0c3a648a
belbeeno <das_garage@hotmail.com>
2018-03-19 04:47:11 -0700

    New site with Factory Hiro and shapes and stuff

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <!DOCTYPE html>
+<!--
+            .';oxOKXNNWWWWNX0xc.                                           
+       .,cok0KNWNOdollcclooxOXXKd'                                         
+  .;cdk00kdlckWMO'           .c0WO.       .::'                             
+o0XX0xc'.    cNMx.            ,0Wx.      .dWMK;        .'cllodxkkxdl:'     
+cl:'.        lWWo           .:0Nx.       lNMMWx.    .okKNMMN0kxddxkKNXk;   
+            .dWNc         'l0NKc.       cXN0XMX;    ,dxokNMO'      .,dXNo. 
+            .kMK;      .:xXXkc.        ;KNo.lNWx.       ;KMk.         cNX: 
+            .OM0'  .'cxKNOo'         .;0No. .OM0,       ;XWo          lN0' 
+            ;KM0c:d0XXOo,.          ckKXl.   lNWd.      cNX:        .dNX:  
+           .oWMWWN0d:.            .lXMNo.    '0MK;      oWK,      .lKNO;   
+           ,KMMMMXd;.            .xNMMNOdooooxKMWXxo'   oWK;    'oKXO:.    
+           .kMWNXNWWKkoc,.     .:0WXkkkkxolc:::xNMNx;.  ;0W0ocoOXKd,       
+           .kMK:.':ox0NWNKkdc,:kWNx'           .c0NX0k:  'oKNN0xc.         
+           '0MO.     .':ok0XNNWMMNkl::;;co;      ..'..     .,'.            
+           cNMk.          ..,:odk0KXXXX0kd:.                               
+          .xMMx.                  ......                          .,;:cloo,
+          .dWWd.                                    .,'.    ,dl. cXWNKOxoc.
+           .;,.                 ...       .cxO0Od. .kWNO;  .xM0,.xWNo...   
+                             .dOKK0Oxd:  'kXOlckXl ;XWNN0, ,KWd.'0MNxodxd; 
+                             .:ood0W0x;.c0O:.  :Xk,oW0cdNk'oWX: ,KWKkdl;.  
+                                .dKx' .cXO,   .oXkcOMk..dKk0M0' ;Od'....''.
+                               :OKl.  .xWd.  'xNK:,0Wd. .dNWNo. c0OkO0XX0d'
+                             .xNKo'.   ;00l:dKW0: .d0:   .ONx. .o0Oxdl:;.  
+                             cNMN0Ox:.  'lxkOx:.   ..     ',.   ..         
+                             :K0o:,,..                                     
+-->
 <html lang="en">
 	<head>
 		<?php
@@ -6,33 +33,65 @@
 		echo $meta_and_links;
 		?>
 		<title>Rad Zone Games</title>
-		<?php
-		echo $javascript;
-		?>
 	</head>
-	<body onload="init()">
+	<body>
 		<?php
-		echo $body_top;
+		echo $javascript
 		?>
-
-		<center>
-			<div style="background-color: #55555555; width: 33%; padding: 10px;">
-				<img src="img/FactoryHiro.gif" style="width:100%;"/>
-				<h2><a href="http://factory.radz.one">Factory Hiro</a> has been released!</h2>
-				<p>Buy it today on <a href="https://itunes.apple.com/us/app/factory-hiro/id1302974264">iOS</a>, <a href="https://play.google.com/store/apps/details?id=com.RadZone.Factory">Android</a>, <a href="https://radzone.itch.io/factory-hiro">Itch.io</a> and <a href="http://store.steampowered.com/app/770820/Factory_Hiro/">Steam</a>!</p>
+		<?php
+		echo $body_shapes;
+		?>
+		
+		<div id="leftbox">
+		  <img src="img/logo.png" class="logo" />
+		  <div class="textbox">
+		    <p>video games by <a href="https://twitter.com/belbeeno">@belbeeno</a> and friends for you.</p>
+		  </div>
+		  <div class="bottombox">
+		    <div class="iconsbox">
+		      <a href="https://twitter.com/radzonegames" class="iconLink"><i class="icon twitterIcon fab fa-twitter"></i></a>
+		      <a href="https://github.com/belbeeno" class="iconLink"><i class="icon githubIcon fab fa-github"></i></a>
+		      <a href="mailto:hello@radz.one" class="iconLink"><i class="icon mailIcon fas fa-envelope"></i></a>
+		    </div>
+		  </div>
+		</div>
+		<!--div id="mainarea" style="background-color: rgba(1, 0, 0, 0.5)"-->
+		<div id="mainarea">
+			<svg height="100%" width="100%" viewBox="0 0 100 40" preserveAspectRatio="xMinYMid" style="position: absolute;">
+				<polygon points="5,10 100,5 80,40 15,30" style="fill:#000;" />
+				<polygon points="3,5 98,0 78,35 13,25" style="fill:rgba(97, 180, 225, 1);" />
+			</svg>
+			<div class="mainEntry aspect-ratio-box">
+				<div class="aspect-ratio-content">
+					<div class="mainEntryContent">
+						<h1>Factory Hiro</h1>
+						<table>
+							<tr>
+								<td>
+									<div class="underthumb" style="background-image: url('img/factory/img01.gif')">
+										<img class="thumbnail" src="img/factory/img00.gif" alt="Factory Hiro Image 1" />
+									</div>
+								</td>
+								<td class="description">
+									<p>
+										Factory Hiro has you playing as Hiro Kikuchi (Age 42) as he oversees an assembly line.  Each day is a new puzzle that will test your reflexes, patience and quick thinking right as it all goes horribly wrong...
+									</p>
+									<p>
+										This game was made by <a href="https://twitter.com/belbeeno">ya boi</a>, as well as art from <a href="https://twitter.com/kcgreenn/">@kcgreenn</a> and <a href="https://twiter.com/randyzero">@randyzero</a>.
+									</p>
+									<div class="description" style="text-align: center;">
+										<p><u>Available on</u>
+											<br/>&#x1F4BE PC and Mac through <a href="http://store.steampowered.com/app/770820/Factory_Hiro/">Steam</a> and <a href="https://radzone.itch.io/factory-hiro">Itch.io</a>
+											<br/><a href="https://play.google.com/store/apps/details?id=com.RadZone.Factory">&#x1F916 Android tablets and phones</a>
+											<br/><a href="https://itunes.apple.com/us/app/factory-hiro/id1302974264">&#x1F34F iPad and iPhone</a>
+										</p>
+									</div>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</div>
 			</div>
-		</center>
-
-		<!-- BODY &autoplay=1 -->
-		<div class="tv-wrapper">
-			<iframe src="https://www.youtube.com/embed/Lf8JgW3TKC0?modestbranding=1&showinfo=0&controls=0&loop=1&playlist=Lf8JgW3TKC0&vq=hd1080&rel=0&autoplay=1" frameborder="0" width="400" height="300"></iframe>
-		</div> <!-- .tv-wrapper -->
-		<!-- /BODY -->
-
-		<?php
-		echo $body_bottom;
-		echo $footer;
-		echo $bootstrap_core
-		?>
+		</div>
 	</body>
 </html>
